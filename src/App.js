167a0ef02526e39272c9bcea56e499dca4bd5d41
Ariel Calipay <83052833+arielcalipay20@users.noreply.github.com>
2023-07-12 01:11:38 +0800

    Todo Progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Todo(props) {
+  const [todo, setTodo] = useState(props.data);
+
+  const addTodo = (name) => {
+    setTodo([...todo, name]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AddTodo handleSubmit={addTodo} />
+      <TodoList data={todo} />
+    </>
   );
 }
 
-export default App;
+function TodoList(props) {
+  const arr = props.data
+  const listItems = arr.map((val, index) => {
+    return <li key={index}>{val}</li>
+  });
+  return (
+    <ol>{listItems}</ol>
+  );
+}
+
+function AddTodo(props) {
+  const [addTodo, setAddTodo] = useState('');
+
+  const handleChange = (e) => {
+    setAddTodo(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    if (addTodo !== '') {
+      props.handleSubmit(addTodo);
+      alert('Successfully add the ' + addTodo);
+      setAddTodo('');
+    }
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type='text' placeholder='add todo' value={addTodo} onChange={handleChange} />
+      <button type='submit'>add</button>
+    </form>
+  );
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Todo from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const todos = [];
 root.render(
   <React.StrictMode>
-    <App />
+    <Todo data={todos} />
   </React.StrictMode>
 );
 
